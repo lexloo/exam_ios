9ac3cb6a8819a2158b5dbf85a2a8dd8cd5ba5c1f
lexloo <lexloo@126.com>
2017-12-21 21:22:20 +0800

    Initial Commit

---
+++ MedicalExam/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MedicalExam
+//
+//  Created by 黄奇 on 21/12/2017.
+//  Copyright © 2017 SmartWall. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
