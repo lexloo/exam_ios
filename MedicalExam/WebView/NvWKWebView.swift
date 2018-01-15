b25e83a8a8fe2cdfbd64ea3bc52a920a42a49909
lexloo <lexloo@126.com>
2018-01-15 23:18:00 +0800

    xxx

--- MedicalExam/WebView/NvWKWebView.swift
+++ MedicalExam/WebView/NvWKWebView.swift
@@ -38,7 +38,7 @@ class NvWKWebView: WKWebView {
 extension NvWKWebView {
     func initClients() {
         loadSdk()
-        registerClientFuncs(module: "qb", funcs: ["getChapterQuestion,startDoQuestion,getDoQuestion,getCommentCount,saveDoQuestion,getDoQuestionInfo"])
+        registerClientFuncs(module: "qb", funcs: ["getChapterQuestion,startDoQuestion,getDoQuestion,getCommentCount,saveDoQuestion,getDoQuestionInfo,showComments"])
         addKVO()
     }
     
