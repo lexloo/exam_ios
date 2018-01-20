b7ef18f9829d3ae68aaf6af6ca443370ecfcd5e0
lexloo <lexloo@126.com>
2018-01-20 20:12:16 +0800

    xxx

--- MedicalExam/Controller/QuestionBank/CommentsViewController.swift
+++ MedicalExam/Controller/QuestionBank/CommentsViewController.swift
@@ -13,21 +13,54 @@ class CommentsViewController: UIViewController {
     var questionGuid: String?
     var webView: NvWKWebView?
     
+    @IBOutlet weak var vWebViewContainer: UIView!
+    @IBOutlet weak var vCommentInput: UIView!
+    @IBOutlet weak var tvInput: UITextView!
+    @IBOutlet weak var lcBottom: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        webView = NvWKWebView(frame: CGRect(x:0, y:60, width:self.view.bounds.size.width, height:self.view.bounds.size.height), uiViewController: self)
-        view.addSubview(webView!)
+        webView = NvWKWebView(frame: CGRect(x:0, y:0, width:self.vWebViewContainer.bounds.width, height:self.vWebViewContainer.bounds.height), uiViewController: self)
+        vWebViewContainer.addSubview(webView!)
         
         let params: [String: String] = ["questionGuid": questionGuid!]
         webView?.loadHtml(name: "html/comments", params: params)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(CommentsViewController.keyboardWillChange(_:)), name: .UIKeyboardWillChangeFrame, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func clickReturn(_ sender: UIButton) {
+    @IBAction func returnClick(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func cancelCommentClick(_ sender: UIButton) {
+        vCommentInput.isHidden = true
+        tvInput.resignFirstResponder()
+    }
+    @IBAction func okCommentClick(_ sender: UIButton) {
+        vCommentInput.isHidden = true
+        tvInput.resignFirstResponder()
+    }
+    @IBAction func commentsOnMeClick(_ sender: UIButton) {
+    }
+    @IBAction func writeCommentClick(_ sender: UIButton) {
+        vCommentInput.isHidden = false
+        tvInput.becomeFirstResponder()
+    }
+    
+    @objc func keyboardWillChange(_ notification: Notification) {
+        if let userInfo = notification.userInfo,
+            let value = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue,
+            let duration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? Double,
+            let curve = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? UInt {
+            let frame = value.cgRectValue
+            let intersection = frame.intersection(self.view.frame)
+            
+            self.lcBottom.constant = -intersection.height
+            UIView.animate(withDuration: duration, delay: 0.0, options: UIViewAnimationOptions(rawValue: curve), animations: {self.view.layoutIfNeeded()}, completion: nil)
+        }
     }
 }
