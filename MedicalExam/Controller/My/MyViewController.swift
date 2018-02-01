190f97f21a9356eee96dbc51b0637865911bd85c
lexloo <lexloo@126.com>
2018-02-01 22:59:17 +0800

    xxx

--- MedicalExam/Controller/My/MyViewController.swift
+++ MedicalExam/Controller/My/MyViewController.swift
@@ -10,12 +10,24 @@ import UIKit
 
 class MyViewController: UITableViewController {
     @IBOutlet weak var vLogo: UIView!
+    @IBOutlet weak var ivAvatar: UIImageView!
     @IBOutlet var tbProp: UITableView!
     
     @IBAction func exitClick(_ sender: UIButton) {
         exit(0)
     }
 
+    private lazy var pickVC: UIImagePickerController = {
+        let pickVC = UIImagePickerController()
+        pickVC.delegate = self
+        pickVC.allowsEditing = true
+        
+        return pickVC
+    }()
+    private var avatarData: Data?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,10 +35,49 @@ class MyViewController: UITableViewController {
         
         vLogo.bounds = CGRect(x: 0, y: 0, width: view.bounds.width, height: 160)
         vLogo.backgroundColor = BaseColor.statusBarColor
-        self.tbProp.tableFooterView = UIView();
+        
+        ivAvatar.layer.cornerRadius = ivAvatar.frame.size.width / 2
+        ivAvatar.clipsToBounds = true
+        
+        tbProp.tableFooterView = UIView();
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+extension MyViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            pickVC.sourceType = .camera
+            self.present(pickVC, animated: true, completion: nil)
+        } else {
+            MessageUtils.alert(viewController: self, message: "camera")
+        }
+    }
+    
+    private func openLibrary() {
+        pickVC.sourceType = .photoLibrary
+        pickVC.allowsEditing = true
+        present(pickVC, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let typeStr = info[UIImagePickerControllerMediaType] as? String {
+            if typeStr == "public.image" {
+                if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+                    if UIImagePNGRepresentation(image) == nil {
+                        avatarData = UIImageJPEGRepresentation(image, 0.8)
+                    } else {
+                        avatarData = UIImagePNGRepresentation(image)
+                    }
+                    
+                    if avatarData != nil {
+                        
+                    }
+                }
+            }
+        }
     }
 }
